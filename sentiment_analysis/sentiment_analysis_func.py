cafbeac8302a2761768d478d72cd15bc6a41011e
jivers-ucsd <jivers@eng.ucsd.edu>
2019-05-21 08:34:37 -0700

    initial draft of sentiment analysis function

---
+++ sentiment_analysis/sentiment_analysis_func.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+def sentiment_analysis(text,bank):
+    '''
+    Takes in a text file path 'text' and a word bank/dictionary 'bank' and 
+    returns a single score for the general sentiment of that file. 
+    
+    Positive numbers are positive sentiment (the larger positive, the better).
+    Negative numbers are negative sentiment (the more negative, the worse).
+    
+    Inputs
+        text(str)
+        
+        bank(dict)
+            Dictionary where the words are the keys and values are the posivitve
+            or negative score attached to that word.
+            
+    Outputs
+        Score(float)
+            Average sentiment for the text file (total sum score/number of words
+            in file).
+    '''
+    ##imports
+    
+    ##input assertions
+    
+    ##body
+    
+    ##output assertions
