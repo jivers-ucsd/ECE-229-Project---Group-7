553e5eef6e51a778623f0608ca3371c2033b57ff
Jessica Ivers <jivers@eng.ucsd.edu>
2020-05-07 09:41:43 -0700

    updated scraper to handle SI units

--- scraping/get_subscribers.py
+++ scraping/get_subscribers.py
@@ -37,22 +37,44 @@ def get_subs(user, t):
     assert isinstance(t, str)
     assert len(t) == 1
     if t == 'u':
-        content = 'https://www.youtube.com/user/'+user+'/videos'
+        #content = 'https://www.youtube.com/user/'+user+'/videos'
+        content = 'https://www.youtube.com/user/'+user
     elif t == 'p':
         print("Can't handle playlists.. Skipping..")
-        return
+        return None
     elif t == 'c':
-        content = 'https://www.youtube.com/channel/'+user+'/videos'
+        #content = 'https://www.youtube.com/channel/'+user+'/videos'
+        content = 'https://www.youtube.com/channel/'+user
     
     r = requests.get(content)
+    if r.status_code == 404:
+        print('Channel/User information Unavailable')
+        return None
     
     soup = bs(r.text,'html.parser')
-    s = str(soup)
-    pattern = '[0-9,]+ subscribers'
-    m = re.search(pattern, s)
-    n = m[0].split()[0]
-    n = int(''.join(n.split(',')))
-    del s
+    button = soup.findAll("span", {"class": "yt-subscription-button-subscriber-count-branded-horizontal subscribed yt-uix-tooltip"})
+    
+    _prefix = {'K': 1e3,    # kilo
+               'M': 1e6,    # mega
+               'G': 1e9,    # giga
+               'T': 1e12,   # tera
+               'P': 1e15,   # peta
+               }
+    
+    n = 0
+    for subs in button:
+        #print('in for')
+        n = subs.get_text()           
+        n = float(n[0:-1])*_prefix[n[-1]]
+        n = round(n)
+    
+    #s = str(soup)
+    #pattern = '[0-9,]+ subscribers'
+    #m = re.search(pattern, s)
+    #n = m[0].split()[0]
+    #n = int(''.join(n.split(',')))
+    #del s
+        
     return n    
 
 if __name__ == '__main__':
