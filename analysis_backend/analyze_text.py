c9486d4cf5114a97ea6b114325e598d628e85c12
kanhabatra <30479269+kanhabatra@users.noreply.github.com>
2020-05-31 20:31:52 -0700

    Add files via upload

---
+++ analysis_backend/analyze_text.py
@@ -0,0 +1,55 @@
+def analyze_text(database, text, genre, metric):
+    '''
+    Purpose: 
+    Analyze the given text and produce color labels for the words as well as generate an overall score based on the
+    given genre and metric
+    
+    Input:
+    database = str; scalar depicting the name of the pickled database used to conduct analysis
+    text = str; scalar depicting the text that needs to be analyzed
+    genre = str; scalar depicting the genre of the content: "cooking", "gaming", "influencers"
+    metric = str; scalar depicting the metric to base the analysis on: "likes_mean", "likes_median",
+             "dislikes_mean", "dislikes_median", "views_mean", "views_median", "polarity", "subjectivity"
+    
+    Output:
+    analysis = list; a list with the same number of elements as number of words in given text, with each
+               corresponding element being the color for that word: "red" means bad, "yellow" means okay, "green"
+               means good and "white" means "Not found" (in database)
+    score_avg = float; average value of the score: float or "Not applicable" (if none of the words matched the
+                database)
+               
+    '''
+    from textblob import TextBlob
+    import pickle
+    import os
+    data = pickle.load(open(os.path.join(os.getcwd()[0: -16], 'data', database + '.p'), 'rb'))
+    Blob = TextBlob(text)
+    scores = []
+    df_genre = data[genre]
+    score_avg = 0
+    counter = 0
+    for word in list(Blob.words):
+        if word in list(df_genre.words):
+            word_index = df_genre.words[df_genre.words==word].index[0]
+            scores.append(df_genre[metric][word_index])
+            score_avg += df_genre[metric][word_index]
+            counter += 1
+        else:
+            scores.append("Not found")
+    if counter > 0:
+        score_avg = score_avg/counter
+    else:
+        score_avg = "Not Applicable"
+    intervals = [df_genre[metric].mean()-df_genre[metric].std(), df_genre[metric].mean()+df_genre[metric].std()]
+    analysis = []
+    for score in scores:
+        if score=="Not found":
+            analysis.append("white")
+            continue
+        if score<=intervals[0]:
+            analysis.append("red")
+        elif score>intervals[0] and score<=intervals[1]:
+            analysis.append("yellow");
+        elif score>intervals[1]:
+            analysis.append("green")
+    return analysis, score_avg
---
+++ analysis_backend/generate_DF.py
@@ -0,0 +1,66 @@
+def generate_DF(year_begin=2015, year_end=2019, output_name='data_combined'):
+    '''
+    Description:
+    Generating the dataframes for the genres and various features based on the raw text files
+    
+    Input:
+    year_begin = int; numerical year from which the analysis is to start
+    year_end = int; numerical year at the end of which the analysis is to terminate
+    output_name = str; scalar depicting the name of the output file in which the database is pickled
+    '''
+    import os
+    import pandas as pd
+    from textblob import TextBlob
+    from datetime import datetime
+    from statistics import mean, median
+    from textblob import Word
+    import pickle
+    genre_list = os.listdir(os.path.join(os.getcwd()[0: -16], 'data'))
+    genre_list_clean = [element for element in genre_list if "dataFrame" in element]
+    month_categories = list(range(1, (year_end-year_begin+1)*12 + 1)) # 2015 - 2019 (both inclusive)
+    data = dict()
+    for genre in genre_list_clean:
+        name = genre[0: genre.find('_dataFrame.txt')]
+        df_input = pd.read_csv(os.path.join(os.getcwd()[0: -16], 'data', genre))
+        df = pd.DataFrame(columns=["words", "frequency_time", "likes", "likes_mean", "likes_median", "dislikes",
+                                   "dislikes_mean", "dislikes_median", "views", "views_mean", "views_median",
+                                   "polarity", "subjectivity"])
+        for n in range(0, len(df_input)):
+            try:
+                date = datetime.strptime(df_input.date[n], "%b %d, %Y")
+            except:
+                continue
+            date_score = (date.year - year_begin)*12 + date.month
+            total_text = str(df_input.title[n]) + " " + str(df_input.description[n])
+            Blob = TextBlob(total_text)
+            Blob = Blob.lower()
+            for word in Blob.words:
+                if len(Word(word).define())==0:
+                    continue
+                if date_score > month_categories[-1] or date_score < month_categories[0]:
+                    continue
+                if word in list(df.words):
+                    word_index = df.words[df.words==word].index[0]
+                    df.frequency_time[word_index][date_score-1]+=1
+                    df.likes[word_index].append(df_input.likes[n])
+                    df.dislikes[word_index].append(df_input.dislikes[n])
+                    df.views[word_index].append(df_input.views[n])
+                else:
+                    freq_t = [0]*len(month_categories)
+                    freq_t[month_categories.index(date_score)] += 1
+                    likes = [df_input.likes[n]];
+                    dislikes = [df_input.dislikes[n]]
+                    views = [df_input.views[n]]
+                    df = df.append(pd.DataFrame([[word, freq_t, likes, 0, 0, dislikes, 0, 0, views,
+                                                   0, 0, TextBlob(word).polarity, TextBlob(word).subjectivity]],
+                                                 columns=list(df.columns)), ignore_index=True)
+        for n in range(0, len(df)):
+            df.loc[n, 'likes_mean'] = mean(df.likes[n])
+            df.loc[n, 'likes_median'] = median(df.likes[n])
+            df.loc[n, 'dislikes_mean'] = mean(df.dislikes[n])
+            df.loc[n, 'dislikes_median'] = median(df.dislikes[n])
+            df.loc[n, 'views_mean'] = mean(df.views[n])
+            df.loc[n, 'views_median'] = median(df.views[n])
+        df.drop(columns=['likes', 'dislikes', 'views'], inplace=True)
+        data[name] = df
+        pickle.dump(data, open(os.path.join(os.getcwd()[0: -16], 'data', output_name + '.p'), 'wb'))
